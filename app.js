b507d86ace79b714d346a1b4a800386f265ba1f9
keerthivasan123 <keerthivasan287@gmail.com>
2019-05-29 23:05:15 +0530

    app.js is added with ejs , db config, connect to mangodb,middlewares ,routes etc.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-//const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
 var express = require('express');
+const mongoose = require('mongoose');
 //setting express
 var app = express();
-//setting ejs
-app.set('view engine','ejs');
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+  
+//middleware on  static files
+app.use(express.static(__dirname + '/asserts'));
 
 // Routes
 app.use('/', require('./routes/index.js'));
