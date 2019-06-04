94a03b33241429c713ef255faa7fe5d13a39661c
keerthivasan123 <keerthivasan287@gmail.com>
2019-06-04 23:31:26 +0530

    Hardly added Login authentication

--- routes/student.js
+++ routes/student.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const { forwardAuthenticated } = require('../config/authforstudent');
+const Student = require('../models/Student');
 //homePage
-router.get('/', forwardAuthenticated , (req, res) => res.render('student/homePage'));
+router.get('/', (req, res) => res.render('student/homePage'));
 //signin
-router.get('/login', forwardAuthenticated, (req, res) => res.render('student/studentLogin'));
+router.get('/login', (req, res) => res.render('student/studentLogin',{
+  isAuthenticated: false,
+  
+}));
 //dashboard
-router.get('/dashboard', (req, res) => res.render('student/dashboard'));
+router.get('/dashboard', (req, res) => res.render('student/dashboard',{
+  isAuthenticated: req.session.isLoggedIn,
+  name : req.param.rollnumber
+})
+
+);
 // Login
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-      successRedirect: '/student/dashboard',
-      failureRedirect: '/student/login',
-      failureFlash: true
-    })(req, res, next);
+  const rollnumber = req.body.rollnumber;
+  const password = req.body.password;
+  Student.findOne({ rollnumber: rollnumber })
+    .then(user => {
+      if (!user) {
+        return res.redirect('login/');
+      }
+      var isMatch = password.localeCompare(user.password);
+      //console.log(isMatch);
+      //console.log(user.password);
+      if (isMatch==0) {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        return req.session.save(err => {
+          console.log(err);
+          res.redirect('dashboard/');
+        });
+        
+      }
+      res.redirect('login/');
+      
   });
+});
 
 // Logout
 router.get('/logout', (req, res) => {
